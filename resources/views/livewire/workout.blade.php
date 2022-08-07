601ccb7c35317f288cfadbf8f4e37c45ef93b6f4
Ruben van Erk <ruben@rubenvanerk.nl>
2022-08-07 21:32:43 +0200

    Fix timer

--- resources/views/livewire/workout.blade.php
+++ resources/views/livewire/workout.blade.php
@@ -10,42 +10,37 @@
         interval: null
     }">
 
-        <button x-show="!start" @click="
-            start = Date.now()
-            current = start
-            interval = setInterval(() => {current = Date.now()}, 10)
-        ">Start
-        </button>
-
-
-        <div x-show="current">
-            <button @click="
-                start = Date.now()
-                current = start
-            ">restart
-            </button>
+        <div class="mb-4">
+            <x-label>Current rest:</x-label>
+            <span x-text="String(Math.floor((current - start) / 1000 / 60)).padStart(2, '0')"></span> :
+            <span x-text="(String(Math.round((current - start) / 1000) % 60)).padStart(2, '0')"></span>
         </div>
 
-        <span x-text="Math.round((current - start) / 1000)"></span>
+        @foreach($exercises as $exerciseSet)
+            @php
+                $exercise = $exerciseSet->first()->exercise
+            @endphp
+            <h2>{{ $exercise }}</h2>
+
+            <div class="flex space-x-3 overflow-scroll">
+                @foreach($exerciseSet as $set)
+                    <button x-transition
+                            class="@if($set->target_reps === $set->completed_reps) bg-emerald-900 @elseif($set->completed_reps !== null) bg-emerald-500 @else bg-gray-800 @endif rounded-full h-14 w-14 flex justify-around items-center flex-col shrink-0"
+                            wire:click="updateCompletedReps({{ $set }})"
+                            @click="
+                                start = Date.now()
+                                current = start
+                                interval = setInterval(() => {current = Date.now()}, 10)
+                            "
+                    >
+                        <span class="text-white -mt-1">{{ $set->completed_reps ?? $set->target_reps }}</span>
+                        <span class="text-white text-xs -mt-6">{{ $set->weight }}kg</span>
+                    </button>
+                @endforeach
+            </div>
+        @endforeach
+
     </div>
-
-    @foreach($exercises as $exerciseSet)
-        @php
-            $exercise = $exerciseSet->first()->exercise
-        @endphp
-        <h2>{{ $exercise }}</h2>
-
-        <div class="flex space-x-3 overflow-scroll">
-            @foreach($exerciseSet as $set)
-                <button x-transition
-                    class="@if($set->target_reps === $set->completed_reps) bg-emerald-900 @elseif($set->completed_reps !== null) bg-emerald-500 @else bg-gray-800 @endif rounded-full h-14 w-14 flex justify-around items-center flex-col shrink-0"
-                    wire:click="updateCompletedReps({{ $set }})">
-                    <span class="text-white -mt-1">{{ $set->completed_reps ?? $set->target_reps }}</span>
-                    <span class="text-white text-xs -mt-6">{{ $set->weight }}kg</span>
-                </button>
-            @endforeach
-        </div>
-    @endforeach
 
     @if($workout->completed_at === null)
         <x-button wire:click="toggleCompletedAt" class="mt-8">Finish</x-button>
