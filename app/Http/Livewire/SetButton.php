ceaba861c8600353127ee5bdea75f5488dc34123
Ruben van Erk <ruben@rubenvanerk.nl>
2022-08-12 15:30:33 +0200

    Fix phpstan errors

--- app/Http/Livewire/SetButton.php
+++ app/Http/Livewire/SetButton.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Set;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class SetButton extends Component
@@ -11,19 +12,19 @@ class SetButton extends Component
 
     public ?int $completedReps = null;
 
-    public function mount()
+    public function mount(): void
     {
         $this->completedReps = $this->set->completed_reps;
     }
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.set-button');
     }
 
     public function updatedCompletedReps(): void
     {
-        if ($this->set->workout->started_at === null) {
+        if ($this->set->workout && $this->set->workout->started_at === null) {
             $this->set->workout->touch('started_at');
         }
 
