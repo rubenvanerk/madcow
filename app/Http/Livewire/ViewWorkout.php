7e835d5b7f7c4e85e44e5da9c030f8babe1abc82
Ruben van Erk <ruben@rubenvanerk.nl>
2022-08-14 17:31:10 +0200

    Protect own workout from other users

--- app/Http/Livewire/ViewWorkout.php
+++ app/Http/Livewire/ViewWorkout.php
@@ -4,14 +4,17 @@ namespace App\Http\Livewire;
 
 use App\Models\Workout;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class ViewWorkout extends Component
 {
     public Workout $workout;
 
-    public function render(): View
+    public function render(Request $request): View
     {
+        abort_if($request->user()->id !== $this->workout->user->id, 403);
+
         $setsByExercise = $this->workout->sets()->get()->groupBy('exercise');
 
         return view('livewire.view-workout', compact('setsByExercise'));
--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Workout extends Model
@@ -26,5 +27,10 @@ class Workout extends Model
     public function sets(): HasMany
     {
         return $this->hasMany(Set::class, 'workout_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
